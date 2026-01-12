001aa126a5229a401e80d2ff0ee34d78f07b5ced
Lukasz Szaszkiewicz <lukasz.szaszkiewicz@gmail.com>
2026-01-12 17:59:10 +0100

    impl

--- main.go
+++ main.go
@@ -5,18 +5,17 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"strings"
 
 	"github.com/go-logr/logr"
 	libraryinputresources "github.com/openshift/multi-operator-manager/pkg/library/libraryinputresources"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/meta"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/klog/v2"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
@@ -54,32 +53,14 @@ var watchDefs = []libraryinputresources.ExactResourceID{
 	},
 }
 
-const (
-	emptyGroupToken  = "_"
-	gvkPartSeparator = "/"
-	gvkNameSeparator = "::"
-)
-
 type DynamicReconciler struct {
 	client.Client
 	Log logr.Logger
 }
 
 func (r *DynamicReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	gvk, objKey, err := parseRequest(req.NamespacedName)
-	if err != nil {
-		return ctrl.Result{}, err
-	}
-
-	log := r.Log.WithValues("gvk", gvk.String(), "name", objKey)
-
-	obj := &unstructured.Unstructured{}
-	obj.SetGroupVersionKind(gvk)
-	if err := r.Get(ctx, objKey, obj); err != nil {
-		return ctrl.Result{}, client.IgnoreNotFound(err)
-	}
-
-	log.Info("observed object", "uid", obj.GetUID(), "resourceVersion", obj.GetResourceVersion())
+	log := r.Log.WithValues("operator", req.Name)
+	log.Info("observed operator")
 	return ctrl.Result{}, nil
 }
 
@@ -116,7 +97,8 @@ func main() {
 	}
 
 	for _, def := range watchDefs {
-		gvk, obj, err := watchFromExactResourceID(mgr.GetRESTMapper(), scheme, def)
+		def := def
+		_, obj, err := watchFromExactResourceID(mgr.GetRESTMapper(), scheme, def)
 		if err != nil {
 			os.Exit(1)
 		}
@@ -136,7 +118,13 @@ func main() {
 			mgr.GetCache(),
 			obj,
 			handler.TypedEnqueueRequestsFromMapFunc(func(ctx context.Context, obj client.Object) []reconcile.Request {
-				return []reconcile.Request{requestFor(gvk, obj)}
+				operatorName := operatorNameFromResource(obj)
+				gvk, err := apiutil.GVKForObject(obj, scheme)
+				if err != nil {
+					gvk = obj.GetObjectKind().GroupVersionKind()
+				}
+				fmt.Printf("enqueue operator=%s from %s %s/%s\n", operatorName, gvk.String(), obj.GetNamespace(), obj.GetName())
+				return []reconcile.Request{requestForOperator(operatorName, obj)}
 			}),
 			preds...,
 		)); err != nil {
@@ -149,41 +137,13 @@ func main() {
 	}
 }
 
-func encodeGVK(gvk schema.GroupVersionKind) string {
-	group := gvk.Group
-	if group == "" {
-		group = emptyGroupToken
-	}
-	return group + gvkPartSeparator + gvk.Version + gvkPartSeparator + gvk.Kind
+func requestForOperator(operatorName string, obj client.Object) reconcile.Request {
+	name := operatorName
+	return reconcile.Request{NamespacedName: client.ObjectKey{Name: name}}
 }
 
-func decodeGVK(encoded string) (schema.GroupVersionKind, error) {
-	parts := strings.Split(encoded, gvkPartSeparator)
-	if len(parts) != 3 {
-		return schema.GroupVersionKind{}, fmt.Errorf("invalid gvk encoding: %q", encoded)
-	}
-	group := parts[0]
-	if group == emptyGroupToken {
-		group = ""
-	}
-	return schema.GroupVersionKind{Group: group, Version: parts[1], Kind: parts[2]}, nil
-}
-
-func requestFor(gvk schema.GroupVersionKind, obj client.Object) reconcile.Request {
-	name := encodeGVK(gvk) + gvkNameSeparator + obj.GetName()
-	return reconcile.Request{NamespacedName: client.ObjectKey{Namespace: obj.GetNamespace(), Name: name}}
-}
-
-func parseRequest(key client.ObjectKey) (schema.GroupVersionKind, client.ObjectKey, error) {
-	parts := strings.SplitN(key.Name, gvkNameSeparator, 2)
-	if len(parts) != 2 {
-		return schema.GroupVersionKind{}, client.ObjectKey{}, fmt.Errorf("request name missing gvk prefix: %q", key.Name)
-	}
-	gvk, err := decodeGVK(parts[0])
-	if err != nil {
-		return schema.GroupVersionKind{}, client.ObjectKey{}, err
-	}
-	return gvk, client.ObjectKey{Namespace: key.Namespace, Name: parts[1]}, nil
+func operatorNameFromResource(obj client.Object) string {
+	return "example-operator"
 }
 
 func watchFromExactResourceID(mapper meta.RESTMapper, scheme *runtime.Scheme, def libraryinputresources.ExactResourceID) (schema.GroupVersionKind, client.Object, error) {
@@ -198,6 +158,7 @@ func watchFromExactResourceID(mapper meta.RESTMapper, scheme *runtime.Scheme, de
 	if err != nil {
 		return schema.GroupVersionKind{}, nil, err
 	}
+	obj.GetObjectKind().SetGroupVersionKind(gvk)
 
 	cobj, ok := obj.(client.Object)
 	if !ok {
