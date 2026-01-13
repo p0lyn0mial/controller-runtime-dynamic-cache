e5737d1fb670992b7af834d27ce1cb6cc41a6043
Lukasz Szaszkiewicz <lukasz.szaszkiewicz@gmail.com>
2026-01-13 10:37:49 +0100

    init klog

--- main.go
+++ main.go
@@ -2,9 +2,16 @@ package main
 
 import (
 	"context"
+	"errors"
 	"flag"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
+
+	"github.com/go-logr/zapr"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 
 	"github.com/go-logr/logr"
 	libraryinputresources "github.com/openshift/multi-operator-manager/pkg/library/libraryinputresources"
@@ -20,7 +27,6 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
-	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	"sigs.k8s.io/controller-runtime/pkg/metrics/server"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
@@ -105,13 +111,17 @@ func (r *DynamicReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 }
 
 func main() {
-	klog.InitFlags(nil)
-	verbose := flag.Bool("verbose", false, "Enable verbose logging")
-	flag.Parse()
-
-	opts := zap.Options{Development: *verbose}
-
-	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
+	config, err := parseConfiguration(flag.CommandLine, os.Args[1:])
+	if err != nil {
+		panic(err)
+	}
+	logger, err := initCustomZapLogger(config.LogLevel, config.LogEncoder)
+	if err != nil {
+		panic(err)
+	}
+	logrLogger := zapr.NewLogger(logger)
+	ctrl.SetLogger(logrLogger.WithName("ctrl"))
+	klog.SetLogger(logrLogger.WithName("klog"))
 
 	scheme := runtime.NewScheme()
 	if err := corev1.AddToScheme(scheme); err != nil {
@@ -206,4 +216,74 @@ func watchFromExactResourceID(mapper meta.RESTMapper, scheme *runtime.Scheme, de
 		return schema.GroupVersionKind{}, nil, fmt.Errorf("type %T does not implement client.Object", obj)
 	}
 	return gvk, cobj, nil
+}
+
+func initCustomZapLogger(level, encoding string) (*zap.Logger, error) {
+	lv := zap.AtomicLevel{}
+
+	i64, err := strconv.ParseInt(level, 10, 8)
+	numericLevel := int8(i64)
+	if err != nil {
+		// not a numeric level, try to unmarshal it as a zapcore.Level ("debug", "info", "warn", "error", "dpanic", "panic", or "fatal")
+		err := lv.UnmarshalText([]byte(strings.ToLower(level)))
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		// numeric level:
+		// 1. configure klog if the numeric log level is negative and the absolute value of the negative numeric value represents the klog level.
+		// 2. configure the atomic zap level based on the numeric value (5..-9).
+
+		var klogLevel int8 = 0
+		if numericLevel < 0 {
+			klogLevel = -numericLevel
+		}
+
+		klogFlagSet := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+		klog.InitFlags(klogFlagSet)
+		if err := klogFlagSet.Set("v", strconv.Itoa(int(klogLevel))); err != nil {
+			return nil, err
+		}
+
+		lv = zap.NewAtomicLevelAt(zapcore.Level(numericLevel))
+	}
+
+	enc := strings.ToLower(encoding)
+	if enc != "json" && enc != "console" {
+		return nil, errors.New("'encoding' parameter can only by either 'json' or 'console'")
+	}
+
+	cfg := zap.Config{
+		Level:             lv,
+		OutputPaths:       []string{"stdout"},
+		DisableCaller:     false,
+		DisableStacktrace: false,
+		Encoding:          enc,
+		EncoderConfig: zapcore.EncoderConfig{
+			MessageKey:  "msg",
+			LevelKey:    "level",
+			EncodeLevel: zapcore.CapitalLevelEncoder,
+			TimeKey:     "time",
+			EncodeTime:  zapcore.ISO8601TimeEncoder,
+		},
+	}
+	return cfg.Build()
+}
+
+type Config struct {
+	LogLevel   string
+	LogEncoder string
+}
+
+// ParseConfiguration fills the 'OperatorConfig' from the flags passed to the program
+func parseConfiguration(fs *flag.FlagSet, args []string) (Config, error) {
+	config := Config{}
+	fs.StringVar(&config.LogLevel, "log-level", "info", "Log level. Available values: debug | info | warn | error | dpanic | panic | fatal or a numeric value from -9 to 5, where -9 is the most verbose and 5 is the least verbose.")
+	fs.StringVar(&config.LogEncoder, "log-encoder", "json", "Log encoder. Available values: json | console")
+
+	if err := fs.Parse(args); err != nil {
+		return Config{}, fmt.Errorf("failed to parse arguments: %w", err)
+	}
+
+	return config, nil
 }
